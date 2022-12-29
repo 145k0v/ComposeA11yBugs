762c14198d43b164af5715a8860fb5ec18d0e10a
Boris Laskov <b.v.laskov@gmail.com>
2022-12-29 14:49:24 +0100

    Add initial focus bug

--- app/src/main/java/com/example/composea11ybugs/screen/InitialFocusFragment.kt
+++ app/src/main/java/com/example/composea11ybugs/screen/InitialFocusFragment.kt
@@ -3,10 +3,10 @@ package com.example.composea11ybugs.screen
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.compose.foundation.clickable
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,21 +14,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import com.example.composea11ybugs.R
 import com.example.composea11ybugs.component.Header
 import com.example.composea11ybugs.ui.theme.ComposeA11yBugsTheme
 
-class StartFragment : Fragment() {
+class InitialFocusFragment : Fragment() {
 
-    private val navigationController: NavController by lazy { findNavController() }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): ComposeView {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): ComposeView {
         return ComposeView(requireContext()).apply {
             setContent { ComposeA11yBugsTheme { Scr() } }
         }
@@ -36,29 +28,44 @@ class StartFragment : Fragment() {
 
     @Composable
     private fun Scr() {
+        BackHandler { findNavController().popBackStack() }
         Scaffold(
-            topBar = { Header(text = "Start menu", onBack = {}) }
+            topBar = { Header(text = "Start menu", onBack = { findNavController().popBackStack() }) }
         ) { innerPadding ->
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
                     .padding(innerPadding)
-                    .verticalScroll(rememberScrollState())
             ) {
-                Item(
-                    text = "Go to HorizontalPager bugs",
-                    onClick = { navigationController.navigate(R.id.horizontalPagerFragment) })
-                Item(text = "Go to initial focus bug",
-                    onClick = { findNavController().navigate(R.id.initialFocusFragment) })
+                Banner()
+                Spacer(modifier = Modifier.height(8.dp))
+                Column {
+                    Item(text = "Item Zero")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    (1..3).forEach {
+                        Item(text = "Item $it")
+                    }
+                }
             }
         }
     }
 
     @Composable
-    private fun Item(text: String, onClick: () -> Unit) {
+    private fun Banner() {
         Box(
             modifier = Modifier
-                .clickable(onClick = onClick)
+                .fillMaxWidth()
+                .padding(16.dp)
+                .border(1.dp, MaterialTheme.colors.primary)
+                .padding(8.dp)
+        ) {
+            Text(text = "Initial element focus bug")
+        }
+    }
+
+    @Composable
+    private fun Item(text: String) {
+        Box(
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
